d970c6f77a8a4f8e26fc401b6a41c3851f32e2d6
NilTor <zpty@outlook.com>
2018-09-10 17:43:27 +0800

    接口文档的接口类定义

---
+++ PddOpenSdk/Sample/PddApiDocHelper.cs
@@ -0,0 +1,9 @@
+namespace Sample
+{
+    /// <summary>
+    /// 拼多多接口获取帮助类
+    /// </summary>
+    public class PddApiDocHelper
+    {
+    }
+}
---
+++ PddOpenSdk/Sample/PddDocListModel.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+
+namespace Sample
+{
+    /// <summary>
+    /// 拼多多接口列表模型
+    /// </summary>
+    public class PddDocListModel
+    {
+        public bool? Success { get; set; }
+        public long? ErrorCode { get; set; }
+        public object ErrorMsg { get; set; }
+        public Result Result { get; set; }
+    }
+
+    public partial class Result
+    {
+        public long? Id { get; set; }
+        public string Name { get; set; }
+        public List<DocList> DocList { get; set; }
+    }
+
+    public partial class DocList
+    {
+        public string ScopeName { get; set; }
+        public long? ScopeStatus { get; set; }
+        public long? CatId { get; set; }
+        public string CatName { get; set; }
+        public string ApiName { get; set; }
+        public string ApiDesc { get; set; }
+        public long? NeedOauth { get; set; }
+        public string UsageScenarios { get; set; }
+        public string CodeExample { get; set; }
+        public string RequestCodeExample { get; set; }
+        public long? Id { get; set; }
+        public long? Status { get; set; }
+        public long? CreatedAt { get; set; }
+        public long? UpdatedAt { get; set; }
+        public long? CreatorId { get; set; }
+        public string CreatorName { get; set; }
+        public List<ParamList> RequestParamList { get; set; }
+        public List<ParamList> ResponseParamList { get; set; }
+        public List<object> ErrorParamList { get; set; }
+        public List<LimiterList> LimiterList { get; set; }
+        public List<object> ApiDocOperateRecordList { get; set; }
+    }
+
+    public partial class LimiterList
+    {
+        public long? TimeRange { get; set; }
+        public long? Times { get; set; }
+        public long? LimiterLevel { get; set; }
+    }
+
+    public partial class ParamList
+    {
+        public long? Id { get; set; }
+        public string ParamName { get; set; }
+        public string ParamType { get; set; }
+        public long? IsMust { get; set; }
+        public string ParamDesc { get; set; }
+        public string Example { get; set; }
+        public long? ParentId { get; set; }
+        public long? ChildrenNum { get; set; }
+        public string ParentParamName { get; set; }
+        public long? Level { get; set; }
+    }
+
+}
