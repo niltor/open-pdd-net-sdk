dffc00be10425c4cb22b241b88bf365848da19db
NilTor <zpty@outlook.com>
2019-03-19 15:51:53 +0800

    测试图片上传

--- PddOpenSdk/Sample/Controllers/AuthController.cs
+++ PddOpenSdk/Sample/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using PddOpenSdk.AspNetCore;
@@ -12,16 +15,18 @@ namespace Sample.Controllers
 {
     public class AuthController : Controller
     {
-
+        readonly IHostingEnvironment _env;
         readonly PddService _pdd;
-
-        public AuthController(PddService pdd)
+        readonly string AccessToken = "2f80862fdd4e40328539593a0af50037d046758c";
+        public AuthController(PddService pdd, IHostingEnvironment env)
         {
             _pdd = pdd;
+            _env = env;
+            PddCommonApi.AccessToken = AccessToken;
         }
         public IActionResult Index()
         {
-            string url = _pdd.AuthApi.GetDDKOAuthUrl();
+            string url = _pdd.AuthApi.GetWebOAuthUrl(PddCommonApi.RedirectUri);
             ViewData["url"] = url;
             return View();
         }
@@ -38,61 +43,12 @@ namespace Sample.Controllers
             return Content(token.AccessToken);
         }
 
+        /// <summary>
+        /// 测试
+        /// </summary>
+        /// <returns></returns>
         public async Task<ActionResult> Test()
         {
-            PddCommonApi.AccessToken = "bf4e43499f864333bfed2272e702575446daad5d";
-            //var model1 = new GetGoodsCatsRequestModel
-            //{
-            //    ParentCatId = 0
-            //};
-            //var result1 = await _pdd.GoodsApi.GetGoodsCatsAsync(model1);
-
-            //var model = new AddGoodsRequestModel
-            //{
-            //    GoodsName = "葡萄",
-            //    GoodsType = 1,
-            //    GoodsDesc = "葡萄串",
-            //    CatId = 1,
-            //    CountryId = 1,
-            //    MarketPrice = 12,
-            //    IsPreSale = false,
-            //    ShipmentLimitSecond = 3600 * 24,
-            //    CostTemplateId = 1000000,
-            //    IsRefundable = true,
-            //    SecondHand = false,
-            //    IsFolt = true,
-            //    SkuList = new System.Collections.Generic.List<AddGoodsRequestModel.SkuListRequestModel>
-            //    {
-            //        new AddGoodsRequestModel.SkuListRequestModel
-            //        {
-            //           ThumbUrl = "https://t00img.yangkeduo.com/goods/images/2019-03-09/dacebcdc-9c26-479c-9174-f3ecf0b579b6.jpg",
-            //           OverseaSku = new AddGoodsRequestModel.SkuListRequestModel.OverseaSkuRequestModel
-            //           {
-            //               MeasurementCode = "123",
-            //               Taxation = 0,
-            //               Specifications = "spe"
-            //           },
-            //           SpecIdList = "[1754889520]",
-            //           Weight = 200,
-            //           Quantity = 10,
-            //           MultiPrice = 12,
-            //           Price = 15,
-            //           LimitQuantity = 10,
-            //           IsOnsale = 1
-            //        }
-            //    },
-            //    CarouselGallery = new System.Collections.Generic.List<string>
-            //    {
-            //        "https://t00img.yangkeduo.com/goods/images/2019-03-09/dacebcdc-9c26-479c-9174-f3ecf0b579b6.jpg",
-            //        "https://t00img.yangkeduo.com/goods/images/2019-03-09/0a1ee8e4-e94e-4d5c-89ab-fe4a4e163ee9.jpg"
-            //    },
-            //    DetailGallery = new System.Collections.Generic.List<string>
-            //    {
-            //        "https://t00img.yangkeduo.com/goods/images/2019-03-09/0a1ee8e4-e94e-4d5c-89ab-fe4a4e163ee9.jpg"
-            //    }
-            //};
-            //var result = await _pdd.GoodsApi.AddGoodsAsync(model);
-
             var model = new GenDdkWeappQrcodeUrlRequestModel
             {
                 PId = "123133",
@@ -101,7 +57,79 @@ namespace Sample.Controllers
 
             var result = await _pdd.DdkApi.GenDdkWeappQrcodeUrlAsync(model);
 
-            return Content(JsonConvert.SerializeObject(result));
+            return Json(result);
+        }
+
+        /// <summary>
+        /// 测试图片上传
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ActionResult> TestImageUpload()
+        {
+            var filePath = Path.Combine(_env.WebRootPath, "images", "logo.png");
+            byte[] bytes = System.IO.File.ReadAllBytes(filePath);
+            string base64 = "data:image/png;base64," + Convert.ToBase64String(bytes);
+
+            var model = new UploadGoodsImageRequestModel
+            {
+                Image = base64
+            };
+            var result = await _pdd.GoodsApi.UploadGoodsImageAsync(model);
+            return Json(base64);
+        }
+
+        /// <summary>
+        /// 上传商品测试
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ActionResult> TestGoodsUpload()
+        {
+            var model = new AddGoodsRequestModel
+            {
+                GoodsName = "葡萄",
+                GoodsType = 1,
+                GoodsDesc = "葡萄串",
+                CatId = 1,
+                CountryId = 1,
+                MarketPrice = 12,
+                IsPreSale = false,
+                ShipmentLimitSecond = 3600 * 24,
+                CostTemplateId = 1000000,
+                IsRefundable = true,
+                SecondHand = false,
+                IsFolt = true,
+                SkuList = new System.Collections.Generic.List<AddGoodsRequestModel.SkuListRequestModel>
+                {
+                    new AddGoodsRequestModel.SkuListRequestModel
+                    {
+                       ThumbUrl = "https://t00img.yangkeduo.com/goods/images/2019-03-09/dacebcdc-9c26-479c-9174-f3ecf0b579b6.jpg",
+                       OverseaSku = new AddGoodsRequestModel.SkuListRequestModel.OverseaSkuRequestModel
+                       {
+                           MeasurementCode = "123",
+                           Taxation = 0,
+                           Specifications = "spe"
+                       },
+                       SpecIdList = "[1754889520]",
+                       Weight = 200,
+                       Quantity = 10,
+                       MultiPrice = 12,
+                       Price = 15,
+                       LimitQuantity = 10,
+                       IsOnsale = 1
+                    }
+                },
+                CarouselGallery = new System.Collections.Generic.List<string>
+                {
+                    "https://t00img.yangkeduo.com/goods/images/2019-03-09/dacebcdc-9c26-479c-9174-f3ecf0b579b6.jpg",
+                    "https://t00img.yangkeduo.com/goods/images/2019-03-09/0a1ee8e4-e94e-4d5c-89ab-fe4a4e163ee9.jpg"
+                },
+                DetailGallery = new System.Collections.Generic.List<string>
+                {
+                    "https://t00img.yangkeduo.com/goods/images/2019-03-09/0a1ee8e4-e94e-4d5c-89ab-fe4a4e163ee9.jpg"
+                }
+            };
+            var result = await _pdd.GoodsApi.AddGoodsAsync(model);
+            return Json(result);
         }
 
         public IActionResult Privacy()
